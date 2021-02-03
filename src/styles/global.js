5bd2a642261cbd55cdae05808079a1741b0cd6b6
azabraao <az.abraao@gmail.com>
2021-02-02 21:47:14 -0300

    add global style

---
+++ src/styles/global.js
@@ -0,0 +1,82 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  /* http://meyerweb.com/eric/tools/css/reset/
+   v2.0 | 20110126
+   License: none (public domain)
+  */
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    line-height: 1;
+    font-size: 100%;
+    font-family: Poppins, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+  img {
+    display: block;
+  	width: 100%;
+  	height: auto;
+  }
+  :root {
+    --white: #fff;
+    --black: #222;
+    --dark: #818181;
+    --primary: #33a0ff;
+    --primary-lighter: #26fff1;
+    --primary-opaque: #23c0e8;
+    --primary-dark: #2358e8;
+    --primary-darker: #2628ff;
+    --primary-gradient: linear-gradient(181deg, #33A0FF, #2628FF);
+    --warning: #ffb133;
+    --warning-dark: #e8b124;
+    --warning-light: #ffd825;
+    --warning-gradient: linear-gradient(131deg, #FFB113, #FFD825);
+    --danger: #ff6f26;
+    --danger-dark: #e88323;
+  }
+`
+export default GlobalStyles
