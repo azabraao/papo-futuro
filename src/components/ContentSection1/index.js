b0a87dc80ee4d7ecc593419a360a773c6cb710a6
azabraao <az.abraao@gmail.com>
2021-02-17 19:24:45 -0300

    try to handle building window unnavailable

--- src/components/ContentSection1/index.js
+++ src/components/ContentSection1/index.js
@@ -1,54 +1,57 @@
-import React, { memo } from "react"
+import React, { memo, useEffect, useState } from "react"
 import Container from "../Container"
 import SideImage from "./SideImage"
 import * as S from "./styled"
 
 const ContentSection1 = () => {
-  const shouldRenderImage = window && window.innerWidth >= 1170
+  const [shouldRenderImage, setShouldRenderImage] = useState(false)
+
+  useEffect(() => {
+    setShouldRenderImage(window.innerWidth >= 1170)
+  }, [])
 
   return (
     <S.ContentSection1Wrapper>
       <Container>
         <div className="ContentSection1Wrapper__inner">
-
-        <article>
-          <h2 className="color-primary">
-            Porque acreditamos que só a{" "}
-            <span className="color-danger">educação</span> é capaz de
-            transformar a realidade das pessoas.
-          </h2>
-          <p>
-            <span className="color-dark">
-              Com a pandemia, as escolas foram fechadas e os alunos tiveram que
-              se adaptar a uma nova realidade: o 
-              <strong>Ensino a Distância</strong>.
-            </span>
-          </p>
-          <p>
-            <span className="color-primary">
-              Mas como superar esse desafio em um país onde 6,6 milhões de
-              estudantes não têm acesso à internet?
-            </span>
-          </p>
-          <a href="#" className="color-danger">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="23.237"
-              height="26.737"
-              viewBox="0 0 23.237 26.737"
-            >
-              <path
-                id="noun_Arrow_Left_2682937"
-                data-name="noun_Arrow Left_2682937"
-                d="M11.616,23.237,1.93,13.543,0,11.612l1.93-1.93L11.616,0l1.93,1.926L5.226,10.247H26.737v2.73H5.226L13.547,21.3Z"
-                transform="translate(0 26.737) rotate(-90)"
-                fill="#ff6f26"
-              />
-            </svg>
-            Saiba como
-          </a>
-        </article>
-        {shouldRenderImage && <SideImage/>}
+          <article>
+            <h2 className="color-primary">
+              Porque acreditamos que só a{" "}
+              <span className="color-danger">educação</span> é capaz de
+              transformar a realidade das pessoas.
+            </h2>
+            <p>
+              <span className="color-dark">
+                Com a pandemia, as escolas foram fechadas e os alunos tiveram
+                que se adaptar a uma nova realidade: o 
+                <strong>Ensino a Distância</strong>.
+              </span>
+            </p>
+            <p>
+              <span className="color-primary">
+                Mas como superar esse desafio em um país onde 6,6 milhões de
+                estudantes não têm acesso à internet?
+              </span>
+            </p>
+            <a href="#" className="color-danger">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="23.237"
+                height="26.737"
+                viewBox="0 0 23.237 26.737"
+              >
+                <path
+                  id="noun_Arrow_Left_2682937"
+                  data-name="noun_Arrow Left_2682937"
+                  d="M11.616,23.237,1.93,13.543,0,11.612l1.93-1.93L11.616,0l1.93,1.926L5.226,10.247H26.737v2.73H5.226L13.547,21.3Z"
+                  transform="translate(0 26.737) rotate(-90)"
+                  fill="#ff6f26"
+                />
+              </svg>
+              Saiba como
+            </a>
+          </article>
+          {shouldRenderImage && <SideImage />}
         </div>
       </Container>
     </S.ContentSection1Wrapper>
