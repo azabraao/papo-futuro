e4e8f8bed29fba2eae5cd109caffce8c897c91e2
azabraao <az.abraao@gmail.com>
2021-03-04 14:20:21 -0300

    add google sheet api data fetching

--- src/components/Achievements/index.js
+++ src/components/Achievements/index.js
@@ -9,9 +9,28 @@ import { Progress } from "antd"
 
 const Achievements = () => {
   const [isDesktop, setIsDesktop] = useState(false)
+  const [metaAlunos, setMetaAluno] = useState(0)
+  const [alunosAjudados, setAlunosAjudados] = useState(0)
+  const [metaArrecadacao, setMetaArrecadacao] = useState(0)
+  const [valorArrecadado, setValorArrecadado] = useState(0)
 
   useEffect(() => {
     setIsDesktop(window.innerWidth >= 1170)
+
+    const fetchSheetsApi = async () => {
+      try {
+        const response = await fetch("http://papofuturo.com.br/v2/api/get-targets.php")
+        const data = await response.json()
+        setAlunosAjudados(data.alunos_ajudados)
+        setValorArrecadado(data.captacao_total)
+        setMetaAluno(data.meta_alunos)
+        setMetaArrecadacao(data.meta_captacao)
+      } catch (e) {
+        return e
+      }
+    }
+
+    fetchSheetsApi()
   }, [])
 
   return (
@@ -32,17 +51,25 @@ const Achievements = () => {
             <div className="Achievements__cards Achievements__column">
               <div className="Card Card--valor">
                 <div className="Card__title">Valor arrecadado</div>
-                <div className="Card__subtitle color-primary">R$ 40.123,00</div>
+                <div className="Card__subtitle color-primary">
+                  {valorArrecadado}
+                </div>
                 <Progress type="circle" percent={75} trailColor="#fff" />
                 <div className="Card__target-text">Nossa meta é</div>
-                <div className="Card__target color-primary">R$ 100.000,00</div>
+                <div className="Card__target color-primary">
+                  {metaArrecadacao}
+                </div>
               </div>
               <div className="Card Card--alunos">
                 <div className="Card__title">alunos ajudados</div>
-                <div className="Card__subtitle color-warning">90</div>
+                <div className="Card__subtitle color-warning">
+                  {alunosAjudados}
+                </div>
                 <Progress type="circle" percent={75} trailColor="#fff" />
                 <div className="Card__target-text">Nossa meta é</div>
-                <div className="Card__target color-warning">120 alunos</div>
+                <div className="Card__target color-warning">
+                  {metaAlunos} alunos
+                </div>
               </div>
             </div>
           </div>
