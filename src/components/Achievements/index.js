fd0c7b2b6de1baead4b792e007639385c79b5574
azabraao <az.abraao@gmail.com>
2021-03-07 15:28:30 -0300

    add achievements section data

--- src/components/Achievements/index.js
+++ src/components/Achievements/index.js
@@ -13,20 +13,50 @@ const Achievements = () => {
   const [alunosAjudados, setAlunosAjudados] = useState(0)
   const [metaArrecadacao, setMetaArrecadacao] = useState(0)
   const [valorArrecadado, setValorArrecadado] = useState(0)
+  const [percentageCaptacao, setPercentageCaptacao] = useState(0)
+  const [percentageAlunos, setPercentageAlunos] = useState(0)
 
   useEffect(() => {
     setIsDesktop(window.innerWidth >= 1170)
 
+    const handleDataReceived = data => {
+      const captacao_total = parseFloat(data.captacao_total.match(/[\d].*/)[0])
+      const meta_captacao = parseFloat(data.meta_captacao.match(/[\d].*/)[0])
+      const percentageCaptacao = (captacao_total / meta_captacao) * 100
+
+      const meta_alunos = parseFloat(data.meta_alunos)
+      const alunos_ajudados = parseFloat(data.alunos_ajudados)
+      const percentageAlunos = (alunos_ajudados / meta_alunos) * 100
+
+      setPercentageCaptacao(percentageCaptacao)
+      setPercentageAlunos(percentageAlunos)
+
+      setAlunosAjudados(data.alunos_ajudados)
+      setValorArrecadado(data.captacao_total)
+      setMetaAluno(data.meta_alunos)
+      setMetaArrecadacao(data.meta_captacao)
+    }
+
     const fetchSheetsApi = async () => {
       try {
-        const response = await fetch("http://papofuturo.com.br/api/get-targets.php")
+        const response = await fetch(
+          "http://papofuturo.com.br/api/get-targets.php"
+        )
         const data = await response.json()
         setAlunosAjudados(data.alunos_ajudados)
         setValorArrecadado(data.captacao_total)
         setMetaAluno(data.meta_alunos)
         setMetaArrecadacao(data.meta_captacao)
+
+        handleDataReceived(data)
       } catch (e) {
-        return e
+        const data = {
+          meta_alunos: "250",
+          meta_captacao: "R$ 100.000,00",
+          alunos_ajudados: "212",
+          captacao_total: " R$ 80.094,03 ",
+        }
+        handleDataReceived(data)
       }
     }
 
@@ -54,7 +84,11 @@ const Achievements = () => {
                 <div className="Card__subtitle color-primary">
                   {valorArrecadado}
                 </div>
-                <Progress type="circle" percent={75} trailColor="#fff" />
+                <Progress
+                  type="circle"
+                  percent={percentageCaptacao}
+                  trailColor="#fff"
+                />
                 <div className="Card__target-text">Nossa meta é</div>
                 <div className="Card__target color-primary">
                   {metaArrecadacao}
@@ -65,7 +99,11 @@ const Achievements = () => {
                 <div className="Card__subtitle color-warning">
                   {alunosAjudados}
                 </div>
-                <Progress type="circle" percent={75} trailColor="#fff" />
+                <Progress
+                  type="circle"
+                  percent={percentageAlunos}
+                  trailColor="#fff"
+                />
                 <div className="Card__target-text">Nossa meta é</div>
                 <div className="Card__target color-warning">
                   {metaAlunos} alunos
